b76c400b2deb8ba9920fdcd5325eea7d901744a5
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-08-31 15:55:54 -0700

    short description of code changes being uploaded

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,19 @@
-// Name: 
+// Name: Willy Reyes-Rendon
 // This program calculates the tax and tip on a restaurant bill.
+#include <iostream>
+
+int main()
+{
+  double Tax, Tip, Total_Bill, Meal_Cost;
+
+  std::cout <<"What is the total meal cost? ";
+  std::cin >> Meal_Cost;
+  std::cout <<"Tax:7.75%\n";
+  std::cin >> Tax;
+  std::cout <<"Tip:20%\n";
+  std::cin >> Tip;
+  std::cout <<"Total_Bill:$56.8487\n";
+  std::cin >> Total_Bill;
+
+  return 0;
+}
